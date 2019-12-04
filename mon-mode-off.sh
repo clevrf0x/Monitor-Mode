dfb98fedd3b991976c03d42a49b7dd48a5dc823c
Favas <52382725+psycho72@users.noreply.github.com>
2019-12-04 09:08:28 +0530

    Add files via upload

---
+++ mon-mode-off.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+clear
+echo -e 'RUN AS ROOT'
+echo -e ''
+echo -e ''
+if [ -f /usr/bin/toilet ]; then
+        toilet -f smblock -F gay MONITOR MODE DISABLE
+        echo -e '<-----------------Coded by PSYCHO72----------------->'
+else
+	sudo apt-get install toilet -y 
+	toilet -f smblock -F gay RUN PROGRAM AGAIN
+	exit 0
+fi
+echo -e ""
+sudo iwconfig
+echo -e ""
+echo -n "Enter Wireless Interface : "
+read interface
+sudo ifconfig $interface down
+sudo airmon-ng check kill
+sudo iwconfig $interface mode managed
+sudo ifconfig $interface up
+echo -e ""
+sudo iwconfig
+sudo service network-manager restart
+
+exit 0
